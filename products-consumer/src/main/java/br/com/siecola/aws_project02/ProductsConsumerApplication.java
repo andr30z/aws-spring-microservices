48cec7bf4dba197c2b3048c7be44ebce04418946
andr30z <andrelp1015@gmail.com>
2023-02-25 00:03:14 -0300

    add products consumer service

---
+++ products-consumer/src/main/java/br/com/siecola/aws_project02/ProductsConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.siecola.aws_project02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsConsumerApplication.class, args);
+	}
+
+}
