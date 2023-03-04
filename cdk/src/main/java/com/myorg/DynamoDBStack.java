c56c773a1a89f4ca667b4ed633313d22918e2191
andr30z <andrelp1015@gmail.com>
2023-03-03 21:18:45 -0300

    add autoscaling to dynamodb

--- cdk/src/main/java/com/myorg/DynamoDBStack.java
+++ cdk/src/main/java/com/myorg/DynamoDBStack.java
@@ -1,14 +1,17 @@
 package com.myorg;
 
+import software.amazon.awscdk.Duration;
 import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
 import software.amazon.awscdk.services.dynamodb.Attribute;
 import software.amazon.awscdk.services.dynamodb.AttributeType;
 import software.amazon.awscdk.services.dynamodb.BillingMode;
+import software.amazon.awscdk.services.dynamodb.EnableScalingProps;
 // import software.amazon.awscdk.Duration;
 // import software.amazon.awscdk.services.sqs.Queue;
 import software.amazon.awscdk.services.dynamodb.Table;
+import software.amazon.awscdk.services.dynamodb.UtilizationScalingProps;
 import software.constructs.Construct;
 
 public class DynamoDBStack extends Stack {
@@ -25,7 +28,7 @@ public class DynamoDBStack extends Stack {
     final StackProps props
   ) {
     super(scope, id, props);
-    productEventsDdb =
+    this.productEventsDdb =
       Table.Builder
         .create(this, "ProductEventsDb")
         .tableName("product-events")
@@ -41,6 +44,33 @@ public class DynamoDBStack extends Stack {
         .timeToLiveAttribute("ttl")
         .removalPolicy(RemovalPolicy.DESTROY)
         .build();
+
+    //auto scaling configs below
+    productEventsDdb
+      .autoScaleReadCapacity(
+        EnableScalingProps.builder().minCapacity(1).maxCapacity(4).build()
+      )
+      .scaleOnUtilization(
+        UtilizationScalingProps
+          .builder()
+          .targetUtilizationPercent(50)
+          .scaleInCooldown(Duration.seconds(30))
+          .scaleOutCooldown(Duration.seconds(30))
+          .build()
+      );
+
+    productEventsDdb
+      .autoScaleWriteCapacity(
+        EnableScalingProps.builder().minCapacity(1).maxCapacity(4).build()
+      )
+      .scaleOnUtilization(
+        UtilizationScalingProps
+          .builder()
+          .targetUtilizationPercent(50)
+          .scaleInCooldown(Duration.seconds(30))
+          .scaleOutCooldown(Duration.seconds(30))
+          .build()
+      );
   }
 
   Table getProductEventsDdb() {
